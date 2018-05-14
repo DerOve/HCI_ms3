41052ad46c30f65aae6fcf9315cc9162aaf37d26
Jan-Ove Wiesner <jan-ove.wiesner@web.de>
2018-05-14 16:04:29 +0200

    Add files via upload

---
+++ main/java/com/example/jan_ove/ms3/kurs_video.java
@@ -0,0 +1,13 @@
+package com.example.jan_ove.ms3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kurs_video extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kurs_video);
+    }
+}
