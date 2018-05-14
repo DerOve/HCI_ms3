48e13417072106807f1844e9bfa294c891407d29
Jan-Ove Wiesner <jan-ove.wiesner@web.de>
2018-05-14 16:03:05 +0200

    Add files via upload

---
+++ res/ms3/challenge.java
@@ -0,0 +1,13 @@
+package com.example.jan_ove.ms3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class challenge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+    }
+}
